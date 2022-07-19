3adfe69cb59a2248fd2f393c27959cd1dde3cc1d
Olivier.r <olivier.reboul@gmail.com>
2022-07-19 17:03:59 +0200

    hide the menu when click

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 const animeNavBar = () => {
     const navBarButton = document.getElementById("navBarButton");
     const menuNavBar = document.getElementById("navbar-default");
+    const navLinks = document.querySelectorAll("a[href^='#']");
 
     navBarButton.addEventListener('click',()=>{ 
         
@@ -10,6 +11,9 @@ const animeNavBar = () => {
         } else {
             menuNavBar.classList.add('hidden');
         }
-    })
+    });
+
+    navLinks.forEach(navlink => navlink.addEventListener('click',() => {menuNavBar.classList.add('hidden');}));
+
 };
 animeNavBar();
